573981a25acda622d4febc33a072b71d0548066c
flyhero <921285003@qq.com>
2019-01-25 23:47:13 +0800

    update

--- noi.openjudge.cn/2.6基本算法之动态规划/2988 求最小编辑距离.cpp
+++ noi.openjudge.cn/2.6基本算法之动态规划/2988 求最小编辑距离.cpp
@@ -9,7 +9,8 @@ const int maxn = 1005;
 // 教程：https://blog.csdn.net/cqbzlytina/article/details/75043804
 // dp[i][j]表示把串a[1..i]变为串b[1..j]的最少操作次数
 // 边界情况：i=0时，增加j个字符变为b，即dp[0][j]=0; j=0时，增加i个字符变为a，即dp[i][0]=0
-// 状态转移：a[i]变为b[j]想要操作次数最少，需要其前一步也操作次数最少，即：dp[i-1][j]+1, dp[i][j-1]+1, 或dp[i-1][j-1]+1(a[i]!=b[j]时)
+// 状态转移：a[i]变为b[j]想要操作次数最少，需要其前一步也操作次数最少
+// 即：dp[i-1][j]+1, dp[i][j-1]+1, 或dp[i-1][j-1]+1(a[i]!=b[j]时)
 
 int main() {
     int n; cin >> n;
