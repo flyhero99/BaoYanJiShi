278888860e07ab9a2b4bfab6a74e66eb60df4613
flyhero <921285003@qq.com>
2019-03-04 13:19:39 +0800

    Create hdu1176 倒着推的数塔.cpp

---
+++ dp/hdu1176 倒着推的数塔.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+using namespace std;
+typedef long long ll;
+const int maxn = 100005;
+const int inf = 0x3f3f3f3f;
+
+int a[maxn][15], dp[maxn][15];
+
+int main() {
+	int n;
+	while(~scanf("%d", &n) && n) {
+		int x, t, minn = 0;
+		memset(dp, 0, sizeof dp);
+		for(int i = 0; i < n; i++) {
+			scanf("%d %d", &x, &t);
+			minn = max(minn, t);
+			dp[t][x]++;
+		}
+		int maxx = -inf;
+		for(int i = minn-1; i >= 0; i--) {
+			for(int j = 0; j <= 10; j++) {
+				dp[i][j] += max(dp[i+1][j+1], max(dp[i+1][j], dp[i+1][j-1]));
+			}
+		}
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < 11; j++) cout << dp[i][j] << ' '; cout << endl;
+		}
+		printf("%d\n", dp[0][5]);
+	}
+	return 0;
+}
